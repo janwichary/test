9a8d5aa14e979a497b4e338e4192956620805fb8
JanWichary <j.wichary@tiplu.de>
2021-12-11 02:29:31 +0100

    qwe

---
+++ Solution1/test/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Xunit;
+
+namespace test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Equal(0,1);
+    }
+    
+    [Theory]
+    [InlineData(1,1)]
+    [InlineData(1,0)]
+    public void Test2(int in1, int in2)
+    {
+        Assert.Equal(in1, in2);
+    }
+}
